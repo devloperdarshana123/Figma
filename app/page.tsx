19f03543566fd9c9313d8d53e577776e22119b10
developerdarshana123 <23115667@hitdoon.com>
2026-03-06 11:32:23 +0530

    gyshiis

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Stats from "./components/Stats";
 import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
 import About from "./components/About";
-import Team from "./components/Team";
 import CTA from "./components/CTA";
 import Contact from "./components/Contact";
 
